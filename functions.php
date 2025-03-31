7f6a8adf9163be6cfda31251461bf34aa0ec46c1
Gustavo Coimbra <whoisgustavolima@gmail.com>
2025-03-30 22:24:53 -0300

    eveliny-jhovana-beauty

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php 
 
+// Enfileirar CSS
 function enqueue_css($path = 'assets/css') {
     $files = new RecursiveIteratorIterator(
         new RecursiveDirectoryIterator($path),
@@ -9,6 +10,20 @@ function enqueue_css($path = 'assets/css') {
         if ($file->isFile() && pathinfo($file, PATHINFO_EXTENSION) === 'css') {
             $relativePath = str_replace('\\', '/', $file->getPathname());
             echo '<link rel="stylesheet" href="' . $relativePath . '">' . PHP_EOL;
+        }
+    }
+}
+
+// Enfileirar JS
+function enqueue_js($path = 'assets/js') {
+    $files = new RecursiveIteratorIterator(
+        new RecursiveDirectoryIterator($path),
+        RecursiveIteratorIterator::LEAVES_ONLY
+    );
+    foreach ($files as $file) {
+        if ($file->isFile() && pathinfo($file, PATHINFO_EXTENSION) === 'js') {
+            $relativePath = str_replace('\\', '/', $file->getPathname());
+            echo '<script src="' . $relativePath . '"></script>' . PHP_EOL;
         }
     }
 }
--- index.php
+++ index.php
@@ -1,10 +1,12 @@
+<?php include('functions.php') ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <?php enqueue_css(); ?>
+    <?php enqueue_css() ?>
 </head>
 <body>
 
@@ -69,6 +71,7 @@
 
     <script src="assets/js/script.js"></script>
     
+    <?php enqueue_js() ?>
 </body>
 </html>
 
