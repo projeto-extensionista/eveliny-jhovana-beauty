8f2e46e6c2a740e631569093cfe43f7f294a152c
Gustavo Coimbra <whoisgustavolima@gmail.com>
2025-03-30 22:14:14 -0300

    eveliny-jhovana-beauty

--- pages/home/banner.php
+++ pages/home/banner.php
@@ -1,20 +1,20 @@
 <section>
-        <div class="container">
+    <div class="container">
+        <div>
+            <h1>Lorem Ipsum Dolor Sit Amet</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pretium  mauris quis dui porta vehicula. Nam turpis nisl, scelerisque quis  molestie id, fringilla id erat. Etiam in est non.</p>
             <div>
-                <h1>Lorem Ipsum Dolor Sit Amet</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean pretium  mauris quis dui porta vehicula. Nam turpis nisl, scelerisque quis  molestie id, fringilla id erat. Etiam in est non.</p>
-                <div>
-                    <a href="#" target="_self">Lorem ipsum</a>
-                    <a href="#" target="_self">
-                        <img src="assets/icons/icon-whatsapp.png" alt="" width="32" height="32">
-                    </a>
-                    <a href="#" target="_self">
-                        <img src="assets/icons/icon-instagram.png" alt="" width="32" height="32">
-                    </a>
-                    <a href="#" target="_self">
-                        <img src="assets/icons/icon-facebook.png" alt="" width="32" height="32">
-                    </a>
-                </div>
+                <a href="#" target="_self">Lorem ipsum</a>
+                <a href="#" target="_self">
+                    <img src="assets/icons/icon-whatsapp.png" alt="" width="32" height="32">
+                </a>
+                <a href="#" target="_self">
+                    <img src="assets/icons/icon-instagram.png" alt="" width="32" height="32">
+                </a>
+                <a href="#" target="_self">
+                    <img src="assets/icons/icon-facebook.png" alt="" width="32" height="32">
+                </a>
             </div>
         </div>
-    </section>
+    </div>
+</section>
