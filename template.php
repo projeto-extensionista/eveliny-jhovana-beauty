9973165ba9e07e929c6a44dcf776ac5efeaf7b56
Gustavo Lima <whoisgustavolima@gmail.com>
2025-04-27 21:35:58 -0300

    eveliny-jhovana-beauty

--- template.php
+++ template.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="pages/home">
+        <link rel="stylesheet" href="pages/<?=$page?>/index.css">
         <title>Evelyn Joanna Beauty</title>
     </head>
     <body>
@@ -14,15 +14,19 @@
     <header class="header">
         <div class="content">
         <a href="/index.php">
-        <img src="/assets/img/Logo preta2 1.png" alt="Logo Evelyn Johane" class="logo">
+            <img src="/assets/img/Logo preta2 1.png" alt="Logo Evelyn Johane" class="logo">
         </a>
         <div class="menu-icon">☰</div>
         </div>
     </header>
 
-    <?php include("$page.php") ?>
+    <?php include("pages/$page/index.html") ?>
 
-    <script src="/assets/js/script.js"></script>
+    <footer>
+
+    </footer>
+
+    <script src="/pages/<?=$page?>/index.js"></script>
     
     </body>
 </html>
