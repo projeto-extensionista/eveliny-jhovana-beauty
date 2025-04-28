b815f9bde93a28fd7e3c1d948b2a6c8c28d8d937
luiz <luizindsc@proton.me>
2025-04-28 18:07:17 -0300

    home page finalizada 2

--- routes.php
+++ routes.php
@@ -2,7 +2,10 @@
     
     $routes = [
         'home',
-        'nome-da-pagina'
+        'penteados',
+        'design-terapeutico',
+        '404',
+        'politica-de-atendimento'
     ];
 
     $page = isset($_GET["page"]) && in_array($_GET["page"], $routes) ? $_GET["page"] : '404';
