064a38b7a1151f8280fa7fb91e5962bb90cab2bc
Gustavo Lima <whoisgustavolima@gmail.com>
2025-04-29 22:04:38 -0300

    eveliny-jhovana-beauty

--- index.php
+++ index.php
@@ -1,21 +1,24 @@
 <?php
 
 class App {
-
     public function run(): void {
-        $this->loadRoutes();
-        $this->loadTemplate();
+        $router = new Router();
+        $page = $router->resolve($_GET['page'] ?? null);
+
+        $this->renderTemplate($page);
     }
 
-    private function loadRoutes(): void {
-        include 'routes.php';
-    }
+    private function renderTemplate(string $page): void {
+        
+        $template = file_get_contents('template.html');
+        $content = file_exists("pages/$page/index.html") ? file_get_contents("pages/$page/index.html") : '';
 
-    private function loadTemplate(): void {
-        include 'template.php';
-    }
+        $output = str_replace(
+            ['{{page}}', '{{content}}'],
+            [$page, $content],
+            $template
+        );
 
+        echo $output;
+    }
 }
-
-$app = new App();
-$app->run();
