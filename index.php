ef2ca1c3f91245afea575f84a7f62c65a6a14b0c
Gustavo Lima <whoisgustavolima@gmail.com>
2025-04-29 22:12:17 -0300

    eveliny-jhovana-beauty

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php 
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+require_once 'App/Router.php';
+require_once 'App/App.php';
 
 $app = new \App\App(); 
 $app->run();
