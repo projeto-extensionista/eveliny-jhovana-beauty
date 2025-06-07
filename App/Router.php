7ad571409d0a195c881eb56e71748f65d0dee345
DiogoSouza <diogohg24@gmail.com>
2025-06-06 21:13:01 -0300

    ceiaçao do rotas de epilaçao e jason e criaçao

--- App/Router.php
+++ App/Router.php
@@ -9,13 +9,9 @@ class Router {
         'penteados',
         'design-terapeutico',
         'design-com-henna',
-<<<<<<< HEAD
-        'maquiagem',
-        'nanopigmentacao'
-=======
         'nanopigmentacao',
-        'desing-com-coloracao'
->>>>>>> bcc549eb4ff8e2ed4a6ceb446beba2782855b8dc
+        'desing-com-coloracao',
+        'epilacao-egipcia'
     ];
 
     private string $default = 'home';
